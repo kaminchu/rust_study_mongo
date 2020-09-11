7c245f8945f26ac8756c9a7826a92503eb8e79c1
kaminchu <kmchu.mail@gmail.com>
2020-09-11 19:17:37 +0900

    web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .service(index)
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
+}
+#[get("/hello")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
 }
